1643d3eef96bd648e0d1386480d6f47a71d4b7ef
Yordan <yordan_yordanov001@abv.bg>
2022-01-10 14:09:35 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -26,10 +26,27 @@ const price = {
 function App() {
   return (
     <div className="App">
-      <div style={wrapper}>
-        <h3 style={mealHeading}>Sushi</h3>
-        <div style={description}>I prefer steak than sushi.</div>
-        <div style={price}>$9.99</div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          margin: "1rem",
+          paddingBottom: "1rem",
+          borderBottom: "1px solid #ccc",
+        }}
+      >
+        <h3 style={{ margin: "0 0 0.25rem 0" }}>Sushi</h3>
+        <div style={{ fontStyle: "italic" }}>I prefer steak than sushi.</div>
+        <div
+          style={{
+            marginTop: "0.25rem",
+            fontWeight: "bold",
+            color: "#ad5502",
+            fontSize: "1.25rem",
+          }}
+        >
+          $9.99
+        </div>
       </div>
     </div>
   );
