a7d81696a2d36591178bdf45d7f56d4b0b88de4f
Yordan <yordan_yordanov001@abv.bg>
2022-01-10 13:36:00 +0200

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
 
+const wrapper = {
+  display: "flex",
+  justifyContent: "space-between",
+  margin: "1rem",
+  paddingBottom: "1rem",
+  borderBottom: "1px solid #ccc",
+};
+
+const mealHeading = {
+  margin: "0 0 0.25rem 0",
+};
+
 function App() {
   return (
-    <div className="App">
-      <div className="wrapper">
-        <h3 className="mealHeading">Sushi</h3>
+    <div style={wrapper}>
+      <div>
+        <h3 style={mealHeading}>Sushi</h3>
         <div className="description">I prefer steak than sushi.</div>
         <div className="price">$9.99</div>
       </div>
