dc5b5a4af31e83c9bed64f7e6ea39c8c678c1ecd
Yordan <yordan_yordanov001@abv.bg>
2022-01-10 13:52:23 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -16,13 +16,20 @@ const description = {
   fontStyle: "italic",
 };
 
+const price = {
+  marginTop: "0.25rem",
+  fontWeight: "bold",
+  color: "#ad5502",
+  fontSize: "1.25rem",
+};
+
 function App() {
   return (
     <div className="App">
       <div style={wrapper}>
         <h3 style={mealHeading}>Sushi</h3>
         <div style={description}>I prefer steak than sushi.</div>
-        <div className="price">$9.99</div>
+        <div style={price}>$9.99</div>
       </div>
     </div>
   );
